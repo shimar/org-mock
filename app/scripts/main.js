ad4df5ad53b4cf7e574bcc36123465d8a8aa8145
shimar <shima01dev@gmail.com>
2015-01-25 22:18:24 +0900

    implement the item panel scripts.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,19 @@
 /* jshint devel:true */
 
+function initTasks() {
+  var tasks = $('task-panel');
+  $('.task-panel .status').bind('click', function(e) {
+    $('i', this).toggleClass('text-success');
+    $('i', this).toggleClass('fa-square-o');
+    $('i', this).toggleClass('fa-check-square-o');
+  });
+
+  $('.task-panel .arrow').bind('click', function(e) {
+    $('i', this).toggleClass('fa-rotate-90');
+    $('.panel-footer', $(this).parents('.task-panel')).toggle();
+  });
+};
+
 $(function() {
-  console.log('\'Allo \'Allo!');
+  initTasks();
 });
