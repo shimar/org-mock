2a9b6860556dc94c3784d1c57791fb9efdcdb379
shimar <shima01dev@gmail.com>
2015-02-12 23:29:14 +0900

    huuuuum.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -33,7 +33,53 @@ function initTasks() {
   });
 };
 
+function initEventItems() {
+  var items = $('.event-item');
+  items.on('dragstart', function(e) {
+    console.log('dragstart', e);
+    this.style.opacity = '0.8';
+    e.originalEvent.dataTransfer.setData('text', e.target.id);
+    // e.preventDefault();
+    // return false;
+  });
+  items.on('dragend', function(e) {
+    console.log('dragend', this);
+    this.style.opacity = '1';
+    e.preventDefault();
+  });
+};
+
+function initDropContainers() {
+  var containers = $('#agenda table tbody tr td.timetable');
+  containers.on('drop', function(e) {
+    var id   = '#' + e.originalEvent.dataTransfer.getData('text');
+    var elem = $(id);
+    // var X = e.layerX;//
+    var y = $(e.target).position().top;
+    // $(this).append(elem);
+    elem.css('top', y + 'px');
+    e.preventDefault();
+  });
+
+  containers.on('dragenter', function(e) {
+    console.log('dragenter', e);
+    e.preventDefault();
+  });
+
+  containers.on('dragover', function(e) {
+    console.log('dragover', e);
+    e.preventDefault();
+  });
+
+  containers.on('dragleave', function(e) {
+    console.log('dragleave', e);
+    e.preventDefault();
+  });
+};
+
 $(function() {
   initTaskForm()
   initTasks();
+  initEventItems();
+  initDropContainers();
 });
